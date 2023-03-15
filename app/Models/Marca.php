8905de022d366ad7eae3cfda4a30f123de180ed5
RenatinhaMacedo <renata.macedo@etec.sp.gov.br>
2023-03-15 20:11:52 -0300

    primeiro comit

---
+++ app/Models/Marca.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Marca extends Model
+{
+    use HasFactory;
+
+    // Indica o nome da tabela do banco de dados
+    protected $table = "marca";
+
+    // Indica os campos que podem ser preenchidos pelo usuário
+    protected $fillable = [
+        'fabricante',
+        'ano',
+        'cor',
+        'carrografia',
+        'foto'
+    ];
+}

23fd0174571a538b0294ce1518e57228887b46a3
Renata Macedo <renata.macedo@etec.sp.gov.br>
2023-03-17 20:29:41 -0300

    alteracoes

---
+++ resources/views/Carros/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <title>Lista de Carros</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Lista de carros</h1>
+
+            <table class="table table-hover table-bordered table-primary">
+                <tr class="table-dark">
+                    <th>Nome</th>
+                    <th>Marca</th>
+                    <th>Data Fabricação</th>
+                    <th>Ano</th>
+                    <th>Cor</th>
+                    <th>Carrografia</th>
+                    <th>Foto</th>
+                </tr>
+
+                @foreach ($carros as $gato)
+                    <tr>
+                        <td>{{ $carro->nome }}</td>
+                        <td>{{ $carro->marca }}</td>
+                        <td>{{ $carro->data_fabricante }}</td>
+                        <td>{{ $carro->ano }}</td>
+                        <td>{{ $carro->cor }}</td>
+                        <td>{{ $carro->carrografia }}</td>
+                        <td>{{ $carro->foto }}</td>
+                        <td></td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+
+
+    </body>
+</html>
